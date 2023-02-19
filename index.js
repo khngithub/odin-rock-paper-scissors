afa7b226ba73df62c1d6abc8a7b3e852e4bb47c4
Khang Nguyen <110009662+khngithub@users.noreply.github.com>
2023-02-19 15:12:13 -0800

    Make the game playable in the webpage
    Three buttons are added to the html representing player's choices.
    Click a button to play against the computer's choice. The result of each
    round is displayed. When a player reaches 5 wins, the final result is
    displayed. The next time any button is pressed, the results of the last
    round are deleted except for the final result.

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+// Game Algorithm
+
 function getComputerChoice () {
     let selection = ['rock', 'paper', 'scissors'];
 
     return selection[Math.floor(Math.random() * 3)];
 }
+
+let playerScore = 0;
+let computerScore = 0;
+let resetFlag = 0;
 
 function playRound (playerSelection, computerSelection) {
     // Make sure playerSelection is all lowercase, and trimmed of white spaces
@@ -24,40 +30,73 @@ function playRound (playerSelection, computerSelection) {
         return 'Draw';
     }
     else if (diff === 1 || diff === -2) {
+        computerScore ++;
         return 'You Lose';
     }
     else if (diff === 2 || diff === -1) {
+        playerScore ++;
         return 'You Win!';
     }
 }
+    
+function keepScore () {
+    if (playerScore == 5 || computerScore == 5) {
+        const div = document.createElement('div');
 
-function game () {
-    // Play for 5 rounds, and keep score
-    let playerScore = 0;
-    let computerScore = 0;
-    let roundNumber = 5;
-    let msg;
-    for (let i = 0; i < roundNumber; i++) {
-        msg = playRound(prompt('Rock, Paper, or Scissors?'), getComputerChoice());
-        console.log(msg);
-
-        if (msg === 'You Win!') {
-            playerScore++;
+        if (playerScore > computerScore) {
+            div.textContent = 'You are the Winner!';
+            div.classList.add('winnerResult');
         }
-        else if (msg === 'You Lose') {
-            computerScore++;
+        else {
+            div.textContent = 'You\'re a Loser :)';
+            div.classList.add('loserResult');
         }
-    }
 
-    // Print final result
-    scoreMsg = `Final score between You and Computer: ${playerScore} vs ${computerScore}.`
-    if (playerScore > computerScore) {
-        console.log(scoreMsg + ' You Win!');
-    }
-    else if (playerScore < computerScore) {
-        console.log(scoreMsg + 'You Lose.');
-    }
-    else {
-        console.log('Game Draw!');
+        // Reset score and add result
+        playerScore = 0;
+        computerScore = 0;
+        document.body.appendChild(div);
+
+        // Raise flag to delete previous round div's
+        resetFlag = 1;
+    };
+}
+
+function resetScore () {
+    if (resetFlag == 1) {
+        const currentScores = document.querySelectorAll('.currentResult');
+
+        // Remove current results' divs
+        for (let i = 0; i < currentScores.length; i++) 
+            currentScores[i].remove();
+        
+        // Reset flag
+        resetFlag = 0;
     }
 }
+
+function pressButton (button) {
+    resetScore();
+    const div = document.createElement('div');
+    div.classList.add('currentResult');
+    div.textContent = playRound(button, getComputerChoice());
+
+    document.body.appendChild(div);
+    keepScore();
+}
+
+// Page Interactivity
+const rockBtn = document.querySelector('#rock');
+rockBtn.addEventListener('click', () => {
+    pressButton('rock');
+});
+
+const paperBtn = document.querySelector('#paper');
+paperBtn.addEventListener('click', () => {
+    pressButton('paper');
+});
+
+const scissorsBtn = document.querySelector('#scissors');
+scissorsBtn.addEventListener('click', () => {
+    pressButton('scissors');
+});
